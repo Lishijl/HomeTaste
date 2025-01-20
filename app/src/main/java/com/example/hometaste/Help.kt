f020b7490bfc02132298d102fd6fac2188e931ef
Christopherjfc <christopherjfc1@gmail.com>
2025-01-20 21:06:59 +0100

    Inflé el bottom navegation bar a las clases Filter, Help, MyRecipies y Settings

--- app/src/main/java/com/example/hometaste/Help.kt
+++ app/src/main/java/com/example/hometaste/Help.kt
@@ -1,21 +1,41 @@
 package com.example.hometaste
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hometaste.databinding.ActivityHelpBinding
 
 class Help : AppCompatActivity() {
+    private lateinit var binding: ActivityHelpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.enableEdgeToEdge()
-        setContentView(R.layout.activity_help)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v: View, insets: WindowInsetsCompat ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHelpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener(bottomNavListener)
+    }
+
+
+    private val bottomNavListener = fun(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                // Cambia a MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+                return true
+            }
+
+            R.id.recetas -> {
+                // Cambia a MyRecipies
+                startActivity(Intent(this, MyRecipies::class.java))
+                return true
+            }
         }
+        return false
     }
 }
--- app/src/main/java/com/example/hometaste/Settings.kt
+++ app/src/main/java/com/example/hometaste/Settings.kt
@@ -1,21 +1,47 @@
 package com.example.hometaste
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hometaste.databinding.ActivitySettingsBinding
 
 class Settings : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v: View, insets: WindowInsetsCompat ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener(bottomNavListener)
+    }
+
+
+    private val bottomNavListener = fun(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                // Cambia a MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+                return true
+            }
+
+            R.id.buscar -> {
+                // Cambia a Filter
+                startActivity(Intent(this, Filter::class.java))
+                return true
+            }
+
+            R.id.recetas -> {
+                // Cambia a MyRecipies
+                startActivity(Intent(this, MyRecipies::class.java))
+                return true
+            }
         }
+        return false
     }
 }
