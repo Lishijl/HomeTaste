9920fba6164771f2bc45876dc4d2a234a30ed3f6
lishi <liulishij@gmail.com>
2025-01-25 18:50:04 +0100

    proveidor de llista de Recipe simulada

--- app/src/main/java/com/example/hometaste/recipies/Recipe.kt
+++ app/src/main/java/com/example/hometaste/recipies/Recipe.kt
@@ -1,11 +1,30 @@
 package com.example.hometaste.recipies
 
+import com.example.hometaste.R
+
+// model de data
 data class Recipe (
-    var idRecipe: Int,
-    var name: String,
-    var description: String,
-    var time: Int,
-    var skillLvl: String,
-    var serving: String,
-    var image: String?
+    val idRecipe: Int,
+    val name: String,
+    val description: String,
+    val time: Int,
+    val skillLvl: String,
+    val serving: Int,
+    val image: Int?
 )
+
+// creem un proveidor per oferir una llista dels objectes Recipe Data
+// accessible amb RecipiesProvider.RecipesList, son com data de simulació,
+// quan encara no tenim api
+class RecipiesProvider{
+    companion object {
+        val RecipesList:List<Recipe> = listOf(
+            Recipe(1, "Croquetas de jamón", "Bechamel, jamoncitos...", 25, "Media", 4,
+                R.drawable.croquetas
+            ),
+            Recipe(2, "Margherita", "Harina, tomate, mozzarella...", 45, "Fácil", 2,
+                R.drawable.margherita
+            )
+        )
+    }
+}
