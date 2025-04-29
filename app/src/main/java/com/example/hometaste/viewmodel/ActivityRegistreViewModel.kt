3d7555ab2de03c8258f02d3bfc852dcdb983d6c6
Christopherjfc <christopherjfc1@gmail.com>
2025-04-29 18:39:55 +0200

    ActivityRegistreViewModel  i proves unitàries del registre finalitzat, inclouen test de nom d'usuari, correu, contrasenya i confirmació de contrasenya.

--- app/src/main/java/com/example/hometaste/viewmodel/ActivityRegistreViewModel.kt
+++ app/src/main/java/com/example/hometaste/viewmodel/ActivityRegistreViewModel.kt
@@ -22,6 +22,10 @@ class ActivityRegistreViewModel {
     private val _errorConfirmPassword = MutableLiveData("")
     val errorConfirmPassword: LiveData<String> = _errorConfirmPassword
 
+    private val _errorEmail = MutableLiveData<String>("")
+    val errorEmail: LiveData<String> = _errorEmail
+
+
 
     // MÉTODOS USERNAME
     fun updateUserName(nomusuari: String) {
@@ -31,9 +35,62 @@ class ActivityRegistreViewModel {
 
     private fun checkUserName() {
         if (_userName.isEmpty()) {
-            _errorNomUsuari.value = "El nombre de usuario es obligatoria"
+            _errorNomUsuari.value = "ERROR: El nombre de campo de usuario no puede estar vacío."
+            return
         }
+
+        if (_userName.length < 5 || _userName.length > 20) {
+            _errorNomUsuari.value =
+                "ERROR: El nombre de usuario debe tener entre 5 y 20 carácteres alfanuméricos."
+            return
+        }
+
+        val usernameRegex =
+            Regex("^[a-zA-Z][a-zA-Z0-9_.-]{4,19}$") // empieza con letra, permite letras, números, _.- y de 5 a 20
+        if (!usernameRegex.matches(_userName)) {
+            if (!_userName[0].isLetter()) {
+                _errorNomUsuari.value = "ERROR: El nombre de usuario debe iniciar con una letra."
+            } else {
+                _errorNomUsuari.value =
+                    "ERROR: El nombre de usuario sólo puede contener carácteres alfanuméricos o “_”, “-” y “.”."
+            }
+            return
+        }
+
+        _errorNomUsuari.value = "Entrada aceptada"
+        validaForm()
     }
+
+
+
+    // MÉTODOS EMAIL
+    fun updateUserEmail(email: String) {
+        _email = email
+        checkUserEmail()
+    }
+
+    private fun checkUserEmail() {
+        if (_email.isEmpty()) {
+            _errorEmail.value = "ERROR: El correo no puede estar vacío."
+            return
+        }
+
+        if ('@' !in _email) {
+            _errorEmail.value = "ERROR: Introduce un correo electrónico válido, que contiene un “@”."
+            return
+        }
+
+        val parts = _email.split("@")
+        if (parts.size != 2 || parts[1].isEmpty() || !parts[1].contains(".")) {
+            _errorEmail.value = "ERROR: Introduce un correo electrónico válido que tenga un dominio."
+            return
+        }
+
+        _errorEmail.value = "Entrada aceptada"
+        validaForm()
+    }
+
+
 
     // MÉTODOS PASSWORD
     fun updateUserPassword(password: String) {
@@ -42,10 +99,6 @@ class ActivityRegistreViewModel {
         checkPasswordsMatch()
     }
 
-    fun updateConfirmUserPassword(password: String) {
-        _userConfirmPassword = password
-        checkPasswordsMatch()
-    }
 
     private fun checkUserPassword() {
         if (_userPassword.isEmpty()) {
@@ -60,6 +113,15 @@ class ActivityRegistreViewModel {
         }
 
         _errorUserPassword.value = "La contraseña pasa los filtros"
+        validaForm()
+    }
+
+
+
+    // MÉTODOS CONFIRM PASSWORD
+    fun updateConfirmUserPassword(password: String) {
+        _userConfirmPassword = password
+        checkPasswordsMatch()
     }
 
     private fun checkPasswordsMatch() {
@@ -70,5 +132,16 @@ class ActivityRegistreViewModel {
         } else {
             _errorConfirmPassword.value = "Las contraseñas si coinciden"
         }
+
+        validaForm()
+    }
+
+    fun validaForm() {
+        val isValid = errorNomUsuari.value == "Entrada aceptada" &&
+                errorUserPassword.value == "La contraseña pasa los filtros" &&
+                errorConfirmPassword.value == "Las contraseñas si coinciden" &&
+                errorEmail.value == "Entrada aceptada"
+
+        _formularivalid.value = isValid
     }
 }
--- app/src/test/java/com/example/hometaste/ActivityRegistreViewModelTest.kt
+++ app/src/test/java/com/example/hometaste/ActivityRegistreViewModelTest.kt
@@ -13,6 +13,81 @@ class ActivityRegistreViewModelTest {
 
     private val viewModel = ActivityRegistreViewModel()
 
+
+    /*
+     * TEST DE USERNAME
+     */
+
+    @Test
+    fun `validar camp d’usuari buit i que retorni l’error corresponent`() {
+        viewModel.updateUserName("")
+        assertEquals("ERROR: El nombre de campo de usuario no puede estar vacío.", viewModel.errorNomUsuari.value)
+    }
+
+    @Test
+    fun `validar camp d’usuari massa curt i que retorni l’error corresponent`() {
+        viewModel.updateUserName("eu")
+        assertEquals("ERROR: El nombre de usuario debe tener entre 5 y 20 carácteres alfanuméricos.", viewModel.errorNomUsuari.value)
+    }
+
+    @Test
+    fun `validar camp d’usuari massa llarg i que retorni l’error corresponent`() {
+        viewModel.updateUserName("supercalifragilisticoespialidoso")
+        assertEquals("ERROR: El nombre de usuario debe tener entre 5 y 20 carácteres alfanuméricos.", viewModel.errorNomUsuari.value)
+    }
+
+    @Test
+    fun `validar camp d’usuari amb caràcters invàlids i que retorni l’error corresponent`() {
+        viewModel.updateUserName("usuariInvalid#123")
+        assertEquals("ERROR: El nombre de usuario sólo puede contener carácteres alfanuméricos o “_”, “-” y “.”.", viewModel.errorNomUsuari.value)
+    }
+
+    @Test
+    fun `validar camp d’usuari que comenci per un carácter que no sigui lletra i que retorni l’error corresponent`() {
+        viewModel.updateUserName("!usuariInvalid")
+        assertEquals("ERROR: El nombre de usuario debe iniciar con una letra.", viewModel.errorNomUsuari.value)
+    }
+
+    @Test
+    fun `validar camp d’usuari correcte`() {
+        viewModel.updateUserName("usuari_Valid")
+        assertEquals("Entrada aceptada", viewModel.errorNomUsuari.value)
+    }
+
+
+    /*
+     * TEST DE EMAIL
+     */
+
+    @Test
+    fun `validar camp email buit i que retorni l’error corresponent`() {
+        viewModel.updateUserEmail("")
+        assertEquals("ERROR: El correo no puede estar vacío.", viewModel.errorEmail.value)
+    }
+
+    @Test
+    fun `validar camp email format incorrecte sense arroba`() {
+        viewModel.updateUserEmail("usuario.gmail.com")
+        assertEquals("ERROR: Introduce un correo electrónico válido, que contiene un “@”.", viewModel.errorEmail.value)
+    }
+
+    @Test
+    fun `validar camp email format incorrecte sense domini`() {
+        viewModel.updateUserEmail("usuario@")
+        assertEquals("ERROR: Introduce un correo electrónico válido que tenga un dominio.", viewModel.errorEmail.value)
+    }
+
+    @Test
+    fun `validar camp email amb format correcte`() {
+        viewModel.updateUserEmail("usuari@gmail.com")
+        assertEquals("Entrada aceptada", viewModel.errorEmail.value)
+    }
+
+
+    /*
+     * TEST DE PASSWORD Y CONFIRM PASSWORD
+     */
+
     @Test
     fun `valida contrasenya i retorna error quan es buida`() {
         viewModel.updateUserPassword("")
@@ -44,6 +119,7 @@ class ActivityRegistreViewModelTest {
     fun `valida contrasenya i retorna error quan no coincideixen`() {
         viewModel.updateUserPassword("MiNombre@123")
         viewModel.updateConfirmUserPassword("Masd!sda232@")
+
         assertEquals("Las contraseñas no coinciden", viewModel.errorConfirmPassword.value)
     }
 
@@ -51,6 +127,21 @@ class ActivityRegistreViewModelTest {
     fun `valida confirmacion contrasenya i retorna correcte quan coincideixen`() {
         viewModel.updateUserPassword("M@yúsculas1")
         viewModel.updateConfirmUserPassword("M@yúsculas1")
+
         assertEquals("Las contraseñas si coinciden", viewModel.errorConfirmPassword.value)
+    }
+
+    /*
+     * VALIDA SI PASAN TODAS LAS PRUEBAS
+     */
+
+    @Test
+    fun `validar formulari complet i correcte`() {
+        viewModel.updateUserName("usuari_Valid")
+        viewModel.updateUserPassword("M@yúsculas1")
+        viewModel.updateConfirmUserPassword("M@yúsculas1")
+        viewModel.updateUserEmail("usuari@gmail.com")
+
+        assertEquals(true, viewModel.formularivalid.value)
     }
 }
